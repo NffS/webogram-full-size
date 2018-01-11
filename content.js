30becb0d07e7b6b3ae03e5cc54f93bece404f104
Lorans Chirko <lorans.chirko@sixa.io>
2018-01-11 16:32:21 +0200

    init commit

---
+++ content.js
@@ -0,0 +1,29 @@
+(function ready() {
+    let html = document.getElementsByTagName('body')[0];
+    html.style.setProperty("font", "15px/18px Tahoma,sans-serif,Arial,Helvetica");
+
+    html = document.getElementsByClassName('tg_head_split')[0];
+    html.style.setProperty("max-width", "85%");
+
+    html = document.getElementsByClassName('tg_head_logo_wrap')[0];
+    html.style.setProperty("width", "40%");
+
+    html = document.getElementsByClassName('tg_head_main_wrap')[0];
+    html.style.setProperty("width", "60%");
+
+    html = document.getElementsByClassName('im_page_wrap')[0];
+    html.style.setProperty("max-width", "85%");
+
+    html = document.getElementsByClassName('im_dialogs_col_wrap')[0];
+    html.style.setProperty("max-width", "25%");
+
+    html = document.getElementsByClassName('im_history_col_wrap')[0];
+    html.style.setProperty("width", "75%");
+
+    html = document.getElementsByClassName('im_send_panel_wrap')[0];
+    html.style.setProperty("max-width", "80%");
+
+    html = document.getElementsByClassName('im_send_form')[0];
+    html.style.setProperty("max-width", "100%");
+
+})();
